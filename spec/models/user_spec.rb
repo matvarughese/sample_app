1dd518c74327fea36c3f13d730fa27c4c0716677
Matt Varughese <matt.varughese@explovia.com>
2013-06-02 16:18:47 +0100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -25,7 +25,9 @@ describe User do
   	it { should respond_to(:password_digest) }
   	it { should respond_to(:password) }
   	it { should respond_to(:password_confirmation) }
+    it { should respond_to(:remember_token) }
   	it { should respond_to(:authenticate) }
+
 
 
 
@@ -38,6 +40,11 @@ describe User do
       @user.email = mixed_case_email
       @user.save
       @user.reload.email.should == mixed_case_email.downcase
+    end
+
+    describe "remember token" do
+      before { @user.save }
+      its(:remember_token) { should_not be_blank }
     end
   end
 
